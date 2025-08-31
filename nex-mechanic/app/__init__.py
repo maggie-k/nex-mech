1115a515094f983421bc9e618ff989b3eda0d451
maggie-k <k.maggiewamboi@gmail.com>
2025-08-31 21:53:10 +0300

    Update __init__.py

--- nex-mechanic/app/__init__.py
+++ nex-mechanic/app/__init__.py
@@ -14,7 +14,7 @@ DB_NAME='database.db'
 def create_app():
 	app= Flask(__name__)
 	app.config.from_object('config')
-	app.config['SECRET_KEY'] = 'neXMeChanic'
+	app.config['SECRET_KEY'] = 'secret'
 	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -54,4 +54,5 @@ def create_app():
 		return UserCarProfile.query.get(int(id))
 
 	return app
+
 
